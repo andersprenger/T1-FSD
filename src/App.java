4fedff962d4c0e0c01bb6f9f23b55222493b03aa
zurawskigabriel <gabrielzds03@gmail.com>
2023-08-20 17:22:14 -0300

    Tarefa 1 pronta

--- src/App.java
+++ src/App.java
@@ -1,11 +1,12 @@
 public class App {
     public static void main(String[] args) {
-        Consultas consultas = new Consultas();
-        consultas.carregaDados();
-        System.out.println("Dia em que mais choveu no ano de 1980: ");
+        FonteDados fonte = new FonteDadosArquivo("poa_temps.txt");                  // usuário passa o nome do arquivo a ser consultado
+        Repositorio repositorio = new Repositorio(fonte);
+        repositorio.carregaDados();
+        /*System.out.println("Dia em que mais choveu no ano de 1980: ");
         System.out.println(consultas.diaQueMaisChoveuNoAno(1980));
         System.out.println("Datas em que choveu mais de 90 milimetros");
         consultas.datasEmQueChouveuMaisDe(90)
-            .forEach(System.out::println);
+            .forEach(System.out::println);*/
     }
 }
