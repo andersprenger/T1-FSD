7d1935aa73a85f96ed2dce487521e98bbecfc215
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2023-08-20 21:32:29 -0300

    rFonteDados to IFonteDados, add IRepositorio, // FonteDadosSistema

--- src/App.java
+++ src/App.java
@@ -1,8 +1,9 @@
 public class App {
     public static void main(String[] args) {
-        FonteDados fonte = new FonteDadosArquivo("poa_temps.txt");                  // usuário passa o nome do arquivo a ser consultado
-        Repositorio repositorio = new Repositorio(fonte);
+        IFonteDados fonte = new FonteDadosArquivo("poa_temps.txt"); // usuário passa o nome do arquivo a ser consultado
+        IRepositorio repositorio = new Repositorio(fonte); // repository com strategy
         repositorio.carregaDados();
+
         Consultas consulta = new Consultas(repositorio.getRegistros());                         // passa os registros extraídos do arquivo (através da classe FonteDados) deixado no objeto repositorio
         System.out.println("Dia em que mais choveu no ano de 1980: ");
         System.out.println(consulta.diaQueMaisChoveuNoAno(1980));
--- src/FonteDadosArquivo.java
+++ src/FonteDadosArquivo.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
-public class FonteDadosArquivo implements FonteDados {
+public class FonteDadosArquivo implements IFonteDados {
     private String nomeArquivo;
 
     public FonteDadosArquivo(String nomeArquivo){
