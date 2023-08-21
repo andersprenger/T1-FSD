990f19da6c978cd86955b654aab5cf20de72fc89
zurawskigabriel <gabrielzds03@gmail.com>
2023-08-20 21:37:05 -0300

    nada ainda

---
+++ src/DataDOT.java
@@ -0,0 +1,31 @@
+public class DataDOT {
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public int getDia(){
+        return dia;
+    }
+
+    public int getMes(){
+        return mes;
+    }
+
+    public int getAno(){
+        return ano;
+    }
+
+    public void setDia(int dia){
+        this.dia = dia;
+    }
+    
+    public void setMes(int mes){
+        this.mes = mes;
+    }
+
+    public void setAno(int ano){
+        this.ano = ano;
+    }
+
+
+}
