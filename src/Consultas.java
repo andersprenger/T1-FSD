2f9627e362b93a76306975ffeb88d31bdcb2bc93
zurawskigabriel <gabrielzds03@gmail.com>
2023-08-20 21:37:00 -0300

    nada aina

--- src/Consultas.java
+++ src/Consultas.java
@@ -13,6 +13,7 @@ public class Consultas {
 
     public Consultas(List<RegistroDoTempo> registros){
         this.registros = registros;
+        
     }
 
     public List<String> datasEmQueChouveuMaisDe(double milimetros){
@@ -31,5 +32,20 @@ public class Consultas {
         .orElseThrow(IllegalArgumentException::new);
         String resp = registro.getDia()+"/"+registro.getMes()+"/"+registro.getAno()+", "+registro.getPrecipitacao();
         return resp;
+    }
+
+    void alteraConsultaPadrao(Predicate consulta){
+
+    }
+
+    /*Tarefa 3
+    Acrescente um método na classe "Consulta" com a seguinte assinatura: "List diasEmQue()". Este método deve retornar a lista de datas em que uma determinada condição se aplica. Esta condição deve ser implementada usando o padrão "strategy". Uma condição padrão deve ser definida no construtor da classe "Consulta", mas deve poder ser alterada por um método com a seguinte assinatura:
+
+    "void alteraConsultaPadrao(Predicate consulta)"
+
+    Crie também a classe "Data", um "DTO" simples que armazena dia, mes e ano. */
+
+    public List diasEmQue() {
+        return 
     }
 }
